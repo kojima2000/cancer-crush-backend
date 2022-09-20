9bdcc8256e2d2230d51e3e8ccfbc06f7db2e61cf
Brady Hammond <bradyhammond@microsoft.com>
2022-09-20 12:04:08 -0400

    Add test endpoint to boilerplate

--- src/cancer_crush/cli.py
+++ src/cancer_crush/cli.py
@@ -5,29 +5,26 @@
 # Created: 09/20/2022                                #
 # Last Edited: 09/20/2022                            #
 # ================================================== #
-#                      IMPORTS                       #
+#                                                    #
 # ================================================== #
 
 import click
 from . import __version__
 from .server.server import start_server
 
-# ================================================== #
-#                     FUNCTIONS                      #
-# ================================================== #
-
-
 @click.command()
 @click.version_option(version=__version__)
 @click.option('--socket', type=str, help="Serves API through specified UNIX "
                                          "domain socket.")
 @click.option('--port', type=int, default=8080, help="Sets a port for API to "
-                                                    "run on.")
+                                                     "run on.")
 def main(socket, port):
     """
     Command line interface for Cancer Crush backend API.
     """
+    print('Starting server on localhost:{port}...'.format(port=port))
     start_server(socket, port)
+    print('Server stopped.' )
 
 # ================================================== #
 #                        EOF                         #
--- src/cancer_crush/server/server.py
+++ src/cancer_crush/server/server.py
@@ -5,18 +5,24 @@
 # Created: 09/20/2022                                #
 # Last Edited: 09/20/2022                            #
 # ================================================== #
-#                      IMPORTS                       #
+#                                                    #
 # ================================================== #
 
 from waitress import serve
 import falcon
 
-# ================================================== #
-#                     FUNCTIONS                      #
-# ================================================== #
+class TestResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
+        resp.text = ('Test Endpoint')
 
 def start_server(socket="", port=8080):
     app = falcon.App()
+    test = TestResource()
+    app.add_route('/test', test)
+
     if not socket:
         serve(app, port=port)
     else:
