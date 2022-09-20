271aebad277adc99c9edc7614f1ad0935e21c403
Brady Hammond <bradyhammond@microsoft.com>
2022-09-20 11:43:00 -0400

    Add boilerplate backend code

---
+++ setup.py
@@ -0,0 +1,47 @@
+# ================================================== #
+#                       SETUP                        #
+# ================================================== #
+# Author: Brady Hammond                              #
+# Created: 09/20/2022                                #
+# Last Edited: 09/20/2022                            #
+# ================================================== #
+#                      IMPORTS                       #
+# ================================================== #
+
+from setuptools import setup, find_packages
+
+# ================================================== #
+#                       SETUP                        #
+# ================================================== #
+
+setup(
+    name='Cancer Crush API',
+    version='0.0.1',
+    license='GPLv3',
+    description='Backend API for Cancer Crush game/learning tool.',
+    author='Brady Hammond, Niharika Tippabhatla',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Games/Entertainment',
+    ],
+    install_requires=['click>=8.1',
+    'falcon>=3.1',
+    'falcon-auth>=1.1',
+    'waitress>=2.1'],
+    entry_points={
+        'console_scripts': ['cc-start = cancer_crush.cli:main'],
+    }
+)
+
+# ================================================== #
+#                        EOF                         #
+# ================================================== #
